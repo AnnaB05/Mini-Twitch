7910f30580cbe99b14467e3489e06f132e96507b
Anna Baker <aebake05@louisville.edu>
2026-07-05 16:55:41 -0400

    readpump added to client.go

--- backend/chat-service/internal/hub/client.go
+++ backend/chat-service/internal/hub/client.go
@@ -14,3 +14,19 @@ type Client struct {
 	//reference to the hub
 	hub *Hub
 }
+
+func (c *Client) ReadPump() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			break
+		}
+
+		//send message to the hub for broadcasting
+		c.hub.broadcast <- message
+	}
+}
