f6e55bc0b5d8f78e1e27041f5645fb9fdebcda24
Anna Baker <aebake05@louisville.edu>
2026-07-11 12:16:30 -0400

    hub integrated into chat service maing.go

--- backend/chat-service/main.go
+++ backend/chat-service/main.go
@@ -5,11 +5,27 @@ import (
 	"net/http"
 
 	"chat-service/cmd/api"
+	"chat-service/internal/hub"
+	WS "chat-service/internal/websocket"
 )
 
 func main() {
+
+	//create the hub
+	hub := hub.NewHub()
+
+	//run the hub
+	go hub.Run()
+
+	//create router
 	router := api.NewRouter()
 
+	//add websocket route to router
+	router.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		WS.ServeWs(hub, w, r)
+	})
+	
+	//start the server
 	log.Println("Chat Service running on :8082")
 	http.ListenAndServe(":8082", router)
 }
